795f603aca444b57fdee058a3e2f463e85654dd8
paddison <patrickbaumann579@gmail.com>
2022-10-15 18:27:23 +0200

    day24 complete

--- src/day_24.rs
+++ src/day_24.rs
@@ -1,13 +1,11 @@
-use std::{ops::Deref, fmt::Display, collections::VecDeque};
-
-type Instruction = (IType, RType, Option<RType>);
+type Instruction = Vec<String>;
 
 pub fn get_solution_1() -> usize {
     let batches = parse_to_batches().into_iter().skip(5).collect::<Vec<Vec<Instruction>>>();
-    let candidates = get_first_five();
-    for (digit, z) in candidates.into_iter() {
-        println!("Digit: {:?}", digit);
+    let digits = FirstFive::new();
+    for (digit, z) in digits.into_iter() {
         if calculate_batch(&batches, z, vec![]) {
+            println!("Digit: {:?}", digit);
             break;
         }
     }
@@ -21,7 +19,6 @@ fn calculate_batch(batches: &[Vec<Instruction>], z: isize, others: Vec<isize>) -
         return true;
     }
     for w in 1..10 {
-        // println!("Batch {} of 14", 15 - batches.len());
         let mut new_others = others.clone();
         new_others.push(w);
         let (n, m) = get_variables(&batches[0]);
@@ -45,8 +42,8 @@ fn parse_to_batches() -> Vec<Vec<Instruction>> {
 }
 
 fn get_variables(batch: &[Instruction]) -> (isize, isize) {
-    match (batch[5].2.unwrap(), batch[15].2.unwrap()) {
-        (RType::Const(n), RType::Const(m)) => (n, m),
+    match (batch[5][2].parse::<isize>(), batch[15][2].parse::<isize>()) {
+        (Ok(n),Ok(m)) => (n, m),
         _ => panic!("Invalid indices"),
     }
 }
@@ -63,136 +60,65 @@ fn try_digit(z: isize, n: isize, m: isize, w: isize) -> Option<isize> {
     }
 }
 
-fn get_first_five() -> Vec<(VecDeque<isize>, isize)> {
-    let mut valids = Vec::new();
-    for n in 11111..100000 {
-        let digits = deque_from_usize(n);
-        if digits.iter().take(5).find(|e| e == &&0).is_some() {
-            continue;
-        }
-        
-        let z = 26_isize.pow(4) * digits[0] + 
-                    26_isize.pow(3) * digits[1] + 
-                    26_isize.pow(2) * digits[2] +
-                    26 * digits[3] +
-                    digits[4] +
+struct FirstFive {
+    n: Numbers
+}
+
+impl FirstFive {
+    fn new() -> Self {
+        Self { n: Numbers::new() }
+    }
+}
+
+impl Iterator for FirstFive {
+    type Item = (usize, isize);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        loop {
+            let n = self.n.next()?;
+            let z = 26_isize.pow(4) * n[0] as isize + 
+                    26_isize.pow(3) * n[1] as isize + 
+                    26_isize.pow(2) * n[2] as isize +
+                    26 * n[3] as isize +
+                    n[4] as isize +
                     2373828;
-        let mod_z = z % 26;
-        if mod_z <= 10 && mod_z >= 2 {
-            valids.push((digits, z));
-        }
-    }
-    println!("{}", valids.len());
-    valids
-}
-
-fn deque_from_usize(mut n: usize) -> VecDeque<isize> {
-
-    let mut deque = VecDeque::new();
-    while n > 0 {
-        deque.push_back((n % 10) as isize);
-        n /= 10;
-    }
-    while deque.len() < 14 {
-        deque.push_back(0);
-    }
-    deque
-}
-
-#[derive(Clone, Copy, Debug)]
-enum RType {
-    W(isize),
-    X(isize),
-    Y(isize),
-    Z(isize),
-    Const(isize),
-}
-
-impl From<RType> for String {
-    fn from(input: RType) -> Self {
-        match input {
-            RType::W(val) => format!("w({})", val),
-            RType::X(val) => format!("x({})", val),
-            RType::Y(val) => format!("y({})", val),
-            RType::Z(val) => format!("z({})", val),
-            RType::Const(val) => format!("{}", val),
+            let mod_z = z % 26;
+            if mod_z <= 10 && mod_z >= 2 {
+                let digit = n[0] as usize * 10000 + 
+                            n[1] as usize * 1000 + 
+                            n[2] as usize * 100 + 
+                            n[3] as usize * 10 + 
+                            n[4] as usize;
+                break Some((digit, z));
+            }
         }
     }
 }
 
-impl Display for RType {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", String::from(*self))
+struct Numbers {
+    n: [u8; 5]
+}
+
+impl Numbers {
+    fn new() -> Self {
+        Self {n: [8, 0, 0, 0, 0]}
     }
 }
 
-impl Deref for RType {
-    type Target = isize;
+impl Iterator for Numbers {
+    type Item = [u8; 5];
 
-    fn deref(&self) -> &Self::Target {
-        match self {
-            RType::W(val) => val,
-            RType::X(val) => val,
-            RType::Y(val) => val,
-            RType::Z(val) => val,
-            RType::Const(val) => val,
-        }
-    }
-}
-
-
-impl From<&str> for RType {
-    fn from(input: &str) -> Self {
-        match input {
-            "w" => RType::W(0),
-            "x" => RType::X(0),
-            "y" => RType::Y(0),
-            "z" => RType::Z(0),
-            n => RType::Const(n.parse::<isize>().unwrap()),
-        }
-    }
-}
-
-#[derive(Clone, Copy, Debug, PartialEq)]
-enum IType {
-    Inp,
-    Add,
-    Mul,
-    Div,
-    Mod,
-    Eql,
-}
-
-impl Display for IType {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let instr: &str = (*self).into();
-        write!(f, "{}", instr)
-    }
-}
-
-impl From<IType> for &str {
-    fn from(input: IType) -> Self {
-        match input {
-            IType::Inp => "inp",
-            IType::Add => "add",
-            IType::Mul => "mul",
-            IType::Div => "div",
-            IType::Mod => "mod",
-            IType::Eql => "eql",
-        }
-    }
-}
-
-impl From<&str> for IType {
-    fn from(input: &str) -> Self {
-        match input {
-            "inp" => IType::Inp,
-            "add" => IType::Add,
-            "mul" => IType::Mul,
-            "div" => IType::Div,
-            "mod" => IType::Mod,
-            "eql" => IType::Eql,
-            _ => unreachable!(),
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.n == [9, 9, 9, 9, 9] {
+            None
+        } else {
+            for i in (0..5).rev() {
+                self.n[i] = (self.n[i]) % 9 + 1;
+                if self.n[i] != 1 {
+                    break;
+                }
+            }
+            Some(self.n)
         }
     }
 }
@@ -201,6 +127,29 @@ fn parse() -> Vec<Instruction> {
     include_str!("../data/day_24.txt")
         .split('\n')
         .map(|line| line.split_whitespace().collect::<Vec<&str>>())
-        .map(|parts| (parts[0].into(), parts[1].into(), parts.get(2).map(|r| (*r).into())))
+        .map(|parts| {
+            let mut instr = Vec::new();
+            match (parts.get(0), parts.get(1), parts.get(2)) {
+                (Some(op), Some(a), Some(b)) => {
+                    instr.push(op.to_string());
+                    instr.push(a.to_string());
+                    instr.push(b.to_string());
+                },
+                (Some(op), Some(a), None) => {
+                    instr.push(op.to_string());
+                    instr.push(a.to_string());
+                },
+                _ => panic!("Invalid input."),
+            }
+            instr
+        })
         .collect()
+}
+
+#[test]
+fn test_number_iter() {
+    let nums = Numbers::new();
+    for n in nums {
+        println!("{:?}", n);
+    }
 }
